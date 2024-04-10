69e4ac9c7bfd326831d3121bc2dda974f91b0935
dharmendra1104 <dharmendra11042@gmail.com>
2024-04-10 20:15:12 +0300

    add js functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let longurl = document.querySelector(".input");
-let unshorted = document.querySelector(".unshorted-url");
-let shorted = document.querySelector(".shorted-url");
+// let unshorted = document.querySelector(".unshorted-url");
+// let shorted = document.querySelector(".shorted-url");
 let btn = document.querySelector(".shorter-btn");
 let shortcontainer = document.querySelector(".short-links");
 let btn2 = document.querySelector(".copy-button");
@@ -12,17 +12,25 @@ async function short() {
     // console.log(data);
     // unshorted.value=longurl.value
     // shorted.innerHTML=data
-
+    
     const linkContainer = document.createElement("div");
     linkContainer.classList.add("link-container");
-
-
+    
+    
     const unshortedInput = document.createElement("input");
     unshortedInput.classList.add("unshorted-url");
     unshortedInput.type = "text";
     unshortedInput.value = longurl.value;
     linkContainer.appendChild(unshortedInput);
-
+    if(unshortedInput.value=== "" ){
+        longurl.style.border=`2px solid red`
+        shortcontainer.appendChild('');
+        const massege=document.createElement("p")
+        massege.innerText=`Please add a link`
+        linkContainer.appendChild(massege);
+    }
+    else{
+    
     const shortPart = document.createElement("span");
     shortPart.classList.add("short-part");
 
@@ -35,15 +43,33 @@ async function short() {
     const copyButton = document.createElement("button");
     copyButton.classList.add("copy-button");
     copyButton.textContent = "Copy";
+        copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(data).then(() => {
+            copyButton.textContent = "Copied!";
+            setTimeout(() => {
+                copyButton.textContent = "Copy";
+            }, 1500);
+        }).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    });
+
+
     shortPart.appendChild(copyButton);
 
     linkContainer.appendChild(shortPart);
 
     shortcontainer.appendChild(linkContainer);
-
+    // console.log('hellp')
+}
 }
 btn.addEventListener('click', short);
 
 
 
+let menubar=document.querySelector(".menu-bar")
+menu.addEventListener('click',()=>{
+    console.log("hello")
+    menubar.classList.toggle('hide')
+})
 
